5cdaedf0d998a360b022c290f007d204e7f9484f
OlkoKS <126245735+OlkoKS@users.noreply.github.com>
2024-01-17 18:57:05 +0100

    added_products_categories_and_list

---
+++ src/components/products/ProductsNotFound/ProductsNotFound.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ProductsNotFoundContainer = styled.div`
+  width: 580px;
+  margin-top: 132px;
+`;
+
+export const SelectedOrangeText = styled.span`
+  color: var(--orange-color);
+`;
