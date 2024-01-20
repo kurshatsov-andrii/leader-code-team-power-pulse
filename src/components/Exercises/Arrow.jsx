9b2db815439d57c0201dd86e1fbccecb48242dfc
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2024-01-20 22:23:24 +0200

    Add modal "well done"

--- src/components/Exercises/Arrow.jsx
+++ src/components/Exercises/Arrow.jsx
@@ -1,4 +1,3 @@
-
 export const Arrow = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 16 16" fill="none" {...props}>
     <path d="M7.5 14L14 7.5M14 7.5L7.5 1M14 7.5H1" stroke="#E6533C" strokeWidth={1.3} strokeLinecap="round" strokeLinejoin="round" />
