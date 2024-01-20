38ca36f5ccd55513289105905125e7e26abd9813
Andrii Kurshatsov <127684850+kurshatsov-andrii@users.noreply.github.com>
2024-01-20 20:34:51 +0200

    Revert "fix"

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -8,7 +8,7 @@ import { refreshUser } from '../../redux/auth/operations';
 const Avatar = () => {
   const dispatch = useDispatch();
   const { userData } = useSelector((state) => state.auth);
-  const loading = useSelector((state) => state.auth.isLoading);
+  const [loading] = useState(false);
   const [imageURL, setImageURL] = useState('');
 
   useEffect(() => {
