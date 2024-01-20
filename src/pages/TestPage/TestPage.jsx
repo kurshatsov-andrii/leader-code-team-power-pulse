c78aabad25e50fdbacaa08b16c06a78741e103c3
RuslanMirasov <info@mirasov.dev>
2024-01-20 20:17:17 +0100

    sdsd

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -2,8 +2,14 @@ import Section from 'components/Section/Section';
 import { Title, TitleWrapper } from 'components/Typography';
 import { ProfileWrapper } from 'components/ProfilePageComponents';
 import { Calendar } from 'components/Forms';
+import { Input } from 'components/Forms';
 
 const UserPage = () => {
+  const Options = [
+    { value: 'all', label: 'All' },
+    { value: 'false', label: 'Recommended' },
+    { value: 'true', label: 'Not recommended' },
+  ];
   return (
     <Section>
       <TitleWrapper>
@@ -12,7 +18,7 @@ const UserPage = () => {
         </Title>
       </TitleWrapper>
       <ProfileWrapper>
-        <Calendar />
+        <Input type="select" name="test" options={Options} onChange={Options} />
       </ProfileWrapper>
     </Section>
   );
