817deeab53f8ce72b1c9caa609943dbeb0eb7dde
andriipkt <spinnevski@ukr.net>
2024-01-20 09:35:41 -0800

    added diary widgets

---
+++ src/components/DiaryWidget/DiaryWidget.jsx
@@ -0,0 +1,105 @@
+import { StatisticsItem, UserStatistics, TextEl, StatisticsItemLable, AlarmWrapper, StatisticsSection } from './DiaryWidget.styled';
+import { Title } from 'components/Typography';
+import Icon from '../Icon/Icon';
+import MediaQuery from 'react-responsive';
+// import { useEffect, useState } from 'react';
+
+const DiaryWidgets = () => {
+  // const [diary, setDiary] = useState();
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const fetchDiaryList = async () => {
+  //     try {
+  //       const data = await dispatch(getDiaryList('17-01-2024'));
+  //       setDiary(data.payload);
+  //       console.log(data.payload);
+  //     } catch (error) {
+  //       console.error('Error fetching diary:', error);
+  //     }
+  //   };
+
+  //   fetchDiaryList();
+  // }, [dispatch]);
+
+  return (
+    <StatisticsSection>
+      <UserStatistics>
+        <StatisticsItem color="var(--orange-color)">
+          <StatisticsItemLable>
+            <Icon name={'knife'} />
+            <TextEl color="rgba(239, 237, 232, 0.80)">Daily calorie intake</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0</Title>
+        </StatisticsItem>
+
+        <StatisticsItem color="var(--orange-color)">
+          <StatisticsItemLable>
+            <Icon name={'dumbbell'} />
+            <TextEl color="rgba(239, 237, 232, 0.80)">Daily physical activity</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0 min</Title>
+        </StatisticsItem>
+
+        <StatisticsItem color="rgba(239, 237, 232, 0.05)">
+          <StatisticsItemLable>
+            <Icon name={'apple'} />
+            <TextEl color="rgba(239, 237, 232, 0.40)">Сalories consumed</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0</Title>
+        </StatisticsItem>
+
+        <StatisticsItem color="rgba(239, 237, 232, 0.05)">
+          <StatisticsItemLable>
+            <Icon name={'fire'} />
+            <TextEl color="rgba(239, 237, 232, 0.40)">Сalories burned</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0</Title>
+        </StatisticsItem>
+
+        <StatisticsItem color="rgba(239, 237, 232, 0.05)">
+          <StatisticsItemLable>
+            <Icon name={'bubbles'} />
+            <TextEl color="rgba(239, 237, 232, 0.40)">Calories remaining</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0</Title>
+        </StatisticsItem>
+
+        <StatisticsItem color={'rgba(239, 237, 232, 0.05)'}>
+          <StatisticsItemLable>
+            <Icon name={'runner'} />
+            <TextEl color="rgba(239, 237, 232, 0.40)">Sports remaining</TextEl>
+          </StatisticsItemLable>
+
+          <Title size="h4">0 min</Title>
+        </StatisticsItem>
+      </UserStatistics>
+
+      <AlarmWrapper>
+        <div style={{ width: '24px', height: '24px' }}>
+          <Icon name={'alarm'} size={24} />
+        </div>
+
+        <MediaQuery maxWidth={767}>
+          <TextEl size="14px" color="rgba(239, 237, 232, 0.30)" $lineheight="1.28">
+            Record all your meals in the calorie diary every day. This will help you be aware of your nutrition and make informed choices.
+          </TextEl>
+        </MediaQuery>
+
+        <MediaQuery minWidth={768}>
+          <TextEl size="16px" color="rgba(239, 237, 232, 0.30)">
+            Record all your meals in the calorie diary every day. This will help you be aware of your nutrition and make informed choices.
+          </TextEl>
+        </MediaQuery>
+      </AlarmWrapper>
+    </StatisticsSection>
+  );
+};
+
+export default DiaryWidgets;
---
+++ src/components/DiaryWidget/DiaryWidget.styled.jsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const StatisticsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  @media screen and (min-width: 768px) {
+    width: 593px;
+    gap: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    gap: 48px;
+    width: 390px;
+  }
+`;
+
+export const UserStatistics = styled.ul`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  gap: 20px 0px;
+
+  @media screen and (min-width: 768px) {
+    gap: 16px;
+    width: 593px;
+    height: 248px;
+    flex-direction: column;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 390px;
+    height: 100%;
+    flex-direction: row;
+  }
+`;
+
+export const StatisticsItemLable = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const AlarmWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+export const StatisticsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 161px;
+  height: 96px;
+
+  padding: 13px;
+
+  border-radius: 12px;
+  border: 1px solid ${(props) => props.bordercolor || 'rgba(239, 237, 232, 0.2)'};
+  background: ${(props) => props.color || 'initial'};
+
+  @media screen and (min-width: 768px) {
+    width: 187px;
+    height: 116px;
+
+    padding: 17px;
+  }
+`;
+
+export const TextEl = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: ${(props) => props.size || '12px'};
+  line-height: ${(props) => props.$lineheight || '1.33'};
+
+  color: ${(props) => props.color || 'rgba(239, 237, 232, 0.8)'};
+
+  @media screen and (max-width: 767px) {
+  }
+
+  @media screen and (min-width: 768px) {
+    line-height: 1.5;
+  }
+`;
