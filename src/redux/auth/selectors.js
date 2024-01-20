ce7f851e22814f4f16d55ece28ff406a5f3c704d
Andrii Kurshatsov <127684850+kurshatsov-andrii@users.noreply.github.com>
2024-01-20 20:37:17 +0200

    Revert "fix"

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,5 @@
 export const selectUserAuthenticated = (state) => state.auth.authenticated;
-export const selectUsers = (state) => state.auth.userData;
+export const selectUserData = (state) => state.auth.userData;
 export const selectUserIsLoading = (state) => state.auth.isLoading;
 export const selectUserError = (state) => state.auth.error;
 export const selectUserToken = (state) => state.auth.token;
