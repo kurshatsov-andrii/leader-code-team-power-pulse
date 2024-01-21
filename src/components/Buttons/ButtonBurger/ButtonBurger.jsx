737adab2fe7b14dbca2fc1a2330e96be5775647c
RuslanMirasov <info@mirasov.dev>
2024-01-21 23:19:15 +0100

    Router fix

--- src/components/Buttons/ButtonBurger/ButtonBurger.jsx
+++ src/components/Buttons/ButtonBurger/ButtonBurger.jsx
@@ -7,6 +7,7 @@ const ButtonBurger = () => {
   const { activeClass, menuToggle } = useMenu();
   const { profile } = useSelector((state) => state.profile);
   const isFilled = profile && Number(profile.blood) > 0 ? true : false;
+
   return (
     <MediaQuery maxWidth={1439}>
       {isFilled && (
