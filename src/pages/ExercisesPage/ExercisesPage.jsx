c6989c38891b5788ad5d987b8760fc1f591d572f
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2024-01-21 20:53:03 +0200

    Update ExercisesPage.jsx

--- src/pages/ExercisesPage/ExercisesPage.jsx
+++ src/pages/ExercisesPage/ExercisesPage.jsx
@@ -2,15 +2,27 @@ import { Outlet } from 'react-router-dom';
 import { Container } from '../../styles/container';
 import { Categories } from '../../components/Exercises';
 import { TitleContent } from '../../components/Typography/Title/Title.styled';
+// import ModalComplete from '../../components/Exercises/modals/ModalComplete';
+import BasicModalWindow from '../../components/BasicModalWindow/BasicModalWindow';
+import ModalTask from '../../components/Exercises/modals/ModalTask';
 
 function ExercisesPage() {
+  const onClick = () => {
+    console.log('clicked.');
+  };
+
   return (
     <Container>
       <section>
-        <TitleContent tag="h1" size="h3">
+        <TitleContent $tag="h1" size="h3">
           Exercises
         </TitleContent>
       </section>
+
+      {/* Modal Well done*/}
+      {/* <BasicModalWindow onClick={onClick}>
+        <ModalComplete onClick={onClick} />
+      </BasicModalWindow> */}
 
       <Categories />
       <Outlet />
