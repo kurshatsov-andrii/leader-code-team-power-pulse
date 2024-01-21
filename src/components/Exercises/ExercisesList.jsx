d30c2925a80bfd4906a2fbcbf94d43b48a178b7e
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2024-01-21 20:53:40 +0200

    Update ExercisesList.jsx

--- src/components/Exercises/ExercisesList.jsx
+++ src/components/Exercises/ExercisesList.jsx
@@ -15,10 +15,13 @@ import {
 } from './ExercisesList.styled';
 import { Arrow } from './Arrow';
 import { ExerciseIcon } from './ExerciseIcon';
+import BasicModalWindow from '../BasicModalWindow/BasicModalWindow';
+import ModalTask from './modals/ModalTask';
 
 export const ExercisesList = () => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectTask, setSelectTask] = useState(null);
 
   const { subcategory } = useParams();
 
@@ -33,52 +36,66 @@ export const ExercisesList = () => {
     return 'loading';
   }
 
+  const onShowModal = (i) => {
+    console.log(data[i]);
+    setSelectTask(data[i]);
+  };
+  const onClick = () => {
+    setSelectTask(null);
+  };
   // equipment: 'barbell';
   // gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0049.gif';
   // time: 3;
 
   return (
-    <ExercisesListWrapper>
-      {data &&
-        data.map(({ _id, name, bodyPart, target, burnedCalories }) => {
-          return (
-            <ExerciseWrapper key={_id}>
-              <ExerciseHeaderWrapper>
-                <CardHeaderTypeWrapper>WORKOUT</CardHeaderTypeWrapper>
-                <CardHeaderButtonWrapper>
-                  <p>Start</p>
-                  <Arrow />
-                </CardHeaderButtonWrapper>
-              </ExerciseHeaderWrapper>
+    <>
+      {data && selectTask && (
+        <BasicModalWindow onClick={onClick}>
+          <ModalTask exerciseTask={selectTask} />
+        </BasicModalWindow>
+      )}
+      <ExercisesListWrapper>
+        {data &&
+          data.map(({ _id, name, bodyPart, target, burnedCalories }, i) => {
+            return (
+              <ExerciseWrapper key={_id}>
+                <ExerciseHeaderWrapper>
+                  <CardHeaderTypeWrapper>WORKOUT</CardHeaderTypeWrapper>
+                  <CardHeaderButtonWrapper onClick={() => onShowModal(i)}>
+                    <p>Start</p>
+                    <Arrow />
+                  </CardHeaderButtonWrapper>
+                </ExerciseHeaderWrapper>
 
-              <div>
-                <NameWrapper>
-                  <ExerciseIcon />
-                  <Name>{name}</Name>
-                </NameWrapper>
+                <div>
+                  <NameWrapper>
+                    <ExerciseIcon />
+                    <Name>{name}</Name>
+                  </NameWrapper>
 
-                <PropertiesWrapper>
-                  <li>
-                    <PropertyName>
-                      Burned calories: <Property>{burnedCalories}</Property>
-                    </PropertyName>
-                  </li>
-                  <li>
-                    <PropertyName>
-                      Body part: <Property>{bodyPart}</Property>
-                    </PropertyName>
-                  </li>
-                  <li>
-                    <PropertyName color="black">
-                      Target: <Property>{target}</Property>
-                    </PropertyName>
-                  </li>
-                </PropertiesWrapper>
-              </div>
-            </ExerciseWrapper>
-          );
-        })}
-    </ExercisesListWrapper>
+                  <PropertiesWrapper>
+                    <li>
+                      <PropertyName>
+                        Burned calories: <Property>{burnedCalories}</Property>
+                      </PropertyName>
+                    </li>
+                    <li>
+                      <PropertyName>
+                        Body part: <Property>{bodyPart}</Property>
+                      </PropertyName>
+                    </li>
+                    <li>
+                      <PropertyName color="black">
+                        Target: <Property>{target}</Property>
+                      </PropertyName>
+                    </li>
+                  </PropertiesWrapper>
+                </div>
+              </ExerciseWrapper>
+            );
+          })}
+      </ExercisesListWrapper>
+    </>
   );
 };
 
